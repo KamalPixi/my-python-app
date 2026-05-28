7c57ef5be7b8e2219b6bbade2a422ac6ca2fe83e
Md Kamal Uddin <60796717+KamalPixi@users.noreply.github.com>
2026-05-28 13:38:03 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World! My first Python Flask app is running.'
+@app.route("/")
+def home():
+    return {
+        "status": "online",
+        "message": "Hello from Flask inside Incus!"
+    }
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
